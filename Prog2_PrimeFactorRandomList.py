763f1c0d56dc67c0ad3fe84400a464e20bc30260
Harsha H <harsha2041889@gmail.com>
2025-07-13 20:03:03 +0530

    feat: Added the 2 function in Prog2

--- Prog2_PrimeFactorRandomList.py
+++ Prog2_PrimeFactorRandomList.py
@@ -1,5 +1,54 @@
 import random
+# Function to get prime factors for the random list and return
+# Dict fo random numbers to prime factors
+def get_random_prime_factors(random_list):
+    get_random_prime_factor_dict = {}
+    for random_number in random_list:
+        get_random_prime_factors = get_prime_factors(random_number)
+        get_random_prime_factor_dict[random_number]= get_random_prime_factors
+    return get_random_prime_factor_dict
 
+# Function to get the prime factors of a number
+def get_prime_factors(number):
+    prime_factors=[]
+    if type(number) is not int or number<2:
+        return prime_factors
+    while number %2 ==0:
+        prime_factors.append(2)
+        number = number//2
+    if number==1:
+        return prime_factors
+    
+    start_range = 3
+    stop_range = number**0.5<3 or number**0.5
+    skip_range = 2
+    # Setting the start, stop and skip value of the range
+    for prime_number in range(start_range, int(stop_range)+1,skip_range):
+        while number %prime_number==0:
+            prime_factors.append(prime_number)
+            number = number//prime_number
+    if number>2:
+        prime_factors.append(number)
+    # Return the prime factors
+    return prime_factors
+
+# Function to get a dict of prime numbers that are factors to list of random numners
+def get_prime_factor_random_list(random_prime_factor_dict):
+    prime_random_dict={}
+    
+    for random_number in random_prime_factor_dict:
+        random_prime_factors = random_prime_factor_dict[random_number]
+
+        unique_prime_factors = set(random_prime_factors)
+
+        for prime_factor in unique_prime_factors:
+            prime_random_list = prime_random_dict.get(prime_factor)
+            if prime_random_list is None:
+                prime_random_list = list()
+                prime_random_dict[prime_factor]= prime_random_list
+            prime_random_list.append(random_number)
+        return prime_random_dict
+    
 # Function to create random List
 def create_random_list():
     # Step 1: Take the input from the user
@@ -23,4 +72,16 @@ def create_random_list():
     
     return random_list
 
-print(create_random_list())
+random_list = create_random_list()
+print(f"Random list generated is {random_list}")
+
+if(len(random_list)>0):
+    random_prime_factor_dict = get_random_prime_factors(random_list)
+
+    prime_random_dict = get_prime_factor_random_list(random_prime_factor_dict)
+
+    print(f"Random Nnumber and its prime factors {random_prime_factor_dict}")
+    print(f"Prime Factor to Random List {prime_random_dict}")
+
+else:
+    print(f"Random List {random_list} is Empty. Enter proper Limits")
